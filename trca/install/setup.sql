b3572b1f9be6a86e52a4859f814974dfd5eacf7e
denisan2875 <33a653847d7c63919d169321eec4a0ae6806ddad>
2020-03-11 19:56:17 -0700

    file modified

--- trca/install/setup.sql
+++
@@ -1,2 +0,0 @@
-REM $Header: 224270.1 setup.sql 11.4.5.0 2012/11/21 carlos.sierra $
-@@tacreate.sql
--- trca/install/uninstall.sql
+++
@@ -1,2 +0,0 @@
-REM $Header: 224270.1 uninstall.sql 11.4.5.0 2012/11/21 carlos.sierra $
-@@tadrop.sql
